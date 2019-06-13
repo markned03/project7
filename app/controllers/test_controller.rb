52a9c5ebbd4244a18a5f735eb84588b2f39be677
administrator <>
2019-06-13 13:15:56 +0200

    Added users to db

---
+++ app/controllers/test_controller.rb
@@ -0,0 +1,10 @@
+class TestController < ApplicationController
+  def index
+  	@imiona = ["ala", "ewa", "ola", "gienia"]
+  	@id = params[:id].to_i
+  end
+def nawigacja
+
+end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  #get 'test/index'
+  match ':controller(/:action(/:id))', :via => [:get, :post]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
