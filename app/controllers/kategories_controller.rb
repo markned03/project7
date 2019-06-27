482a72e7227bb95e6b2a195bd382f8e68042aa47
Marek Nedzusiak <markned@wp.pl>
2019-06-27 18:10:20 +0200

    modified controller kategorie and views

--- app/controllers/kategories_controller.rb
+++ app/controllers/kategories_controller.rb
@@ -7,14 +7,50 @@ class KategoriesController < ApplicationController
   end
 
   def pokaz
+    @kategoria = Kategorie.find(params[:id])
   end
 
   def nowa
+    @kategoria = Kategorie.new(:nazwa => "Podaj nazwę kategroii")
+    @licznik = Kategorie.count + 1
+  end
+  def utworz
+    @kategorie = Kategorie.new(kategorie_parametry)
+      if @kategorie.save
+        redirect_to(:action => 'index')
+      else
+        @licznik = Kategorie.count + 1
+        render('nowa')
+    end
   end
 
   def edycja
+        @kategoria = Kategorie.find(params[:id])
+        @licznik = Kategorie.count
+
+  end
+
+  def aktualizuj
+      @kategoria = Kategorie.find(params[:id])
+        if @kategoria.update_attributes(kategorie_parametry)
+        redirect_to(:action => 'pokaz', :id => @kategoria.id)
+        else
+        @licznik = Kategorie.count
+        render('edycja')
+    end
   end
 
   def usun
+        @kategoria = Kategorie.find(params[:id])
+
+  end
+  def kasuj
+        kategoria = Kategorie.find(params[:id]).destroy
+        redirect_to(:action => 'index')
+  end
+    
+
+  def kategorie_parametry
+    params.require(:kategoria).permit(:nazwa, :pozycja, :widoczna, :created_at)
   end
 end
