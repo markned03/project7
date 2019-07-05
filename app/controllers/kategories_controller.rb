965c9276d3dc045b05105aa147072dc652af647c
Marek Nedzusiak <markned@wp.pl>
2019-07-05 18:26:23 +0200

    Add flash to views kategories

--- app/controllers/kategories_controller.rb
+++ app/controllers/kategories_controller.rb
@@ -11,12 +11,13 @@ class KategoriesController < ApplicationController
   end
 
   def nowa
-    @kategoria = Kategorie.new(:nazwa => "Podaj nazwę kategroii")
+    @kategoria = Kategorie.new(:nazwa => "Podaj nazwę kategorii")
     @licznik = Kategorie.count + 1
   end
   def utworz
     @kategorie = Kategorie.new(kategorie_parametry)
       if @kategorie.save
+        flash[:notice] = "Kategoria została pomyślnie utworzona"
         redirect_to(:action => 'index')
       else
         @licznik = Kategorie.count + 1
@@ -33,6 +34,7 @@ class KategoriesController < ApplicationController
   def aktualizuj
       @kategoria = Kategorie.find(params[:id])
         if @kategoria.update_attributes(kategorie_parametry)
+        flash[:notice] = "Kategoria została pomyślnie zmodyfikowana"
         redirect_to(:action => 'pokaz', :id => @kategoria.id)
         else
         @licznik = Kategorie.count
@@ -46,6 +48,7 @@ class KategoriesController < ApplicationController
   end
   def kasuj
         kategoria = Kategorie.find(params[:id]).destroy
+        flash[:notice] = "Kategoria została pomyślnie usunięta"
         redirect_to(:action => 'index')
   end
     
